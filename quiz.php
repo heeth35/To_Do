8c9ddfda0f8c2478821329020b1904806713b212
heeth35 <66851025+heeth35@users.noreply.github.com>
2020-09-24 01:46:48 +0530

    Add files via upload

---
+++ quiz.php
@@ -0,0 +1,160 @@
+<html>
+<head>
+	<title>Question Paper</title>
+	
+	<style>
+		
+		#indent
+		{
+			padding-left:40px;
+		}
+		
+		#container
+		{
+			padding-left:70px;
+		}
+		
+	</style>
+	
+</head>
+
+<body>
+	<div id = "container" >
+	
+		<h1>Welcome to Vocabulary Quiz</h1>
+		
+		<form action = "answers.php" method = "POST" target="_blank">
+
+			<p>1. The prince abdicated the crown and returned to his castle. Abdicated means…</p>
+				
+				<section id="indent">
+					
+					<input type = "radio" id = "q1" name = "q1" value = "Gave Up"/>GaveUp.<br>
+					<input type = "radio" id = "q1" name = "q1" value = "Sold"/>Sold.<br>
+					<input type = "radio" id = "q1" name = "q1" value = "Reinvested into"/>Reinvested into.<br>
+					<input type = "radio" id = "q1" name = "q1" value = "Auctioned"/>Auctioned.<br>
+				
+				</section>
+				<br>
+
+			<p>2. The convicted criminal absconded prior to the sentencing phase of the trial. Absconded means…</p>
+				
+				<section id="indent">
+					
+					<input type = "radio" id = "q2" name = "q2" value = "Touched the jury."/>Touched the jury.<br>
+					<input type = "radio" id = "q2" name = "q2" value = "Reported immediately."/>Reported immediately.<br>
+					<input type = "radio" id = "q2" name = "q2" value = "Left after discussion."/>Left after discussion.<br>
+					<input type = "radio" id = "q2" name = "q2" value = "Departed secretly."/>Departed secretly.<br>
+							
+				</section>
+				<br>
+
+			<p>3. The aural component of balance is critical for postural control during ambulation. Aural means related to the…</p>
+				
+				<section id="indent">			
+				
+					<input type = "radio" id = "q3" name = "q3" value = "Eye."/>Eye.<br>
+					<input type = "radio" id = "q3" name = "q3" value = "Ear."/>Ear.<br>
+					<input type = "radio" id = "q3" name = "q3" value = "Nose."/>Nose.<br>
+					<input type = "radio" id = "q3" name = "q3" value = "Mouth."/>Mouth.<br>
+				
+				</section>
+				<br>
+
+			<p>4. The old man was benevolent with his fortune. Benevolent means…</p>
+				
+				<section id="indent">
+								
+					<input type = "radio" id = "q4" name = "q4" value = "Positive pole."/>Positive pole.<br>
+					<input type = "radio" id = "q4" name = "q4" value = "Stingy."/>Stingy.<br>
+					<input type = "radio" id = "q4" name = "q4" value = "Kind."/>Kind.<br>
+					<input type = "radio" id = "q4" name = "q4" value = "Careful."/>Careful.<br>
+				
+				</section>
+				<br>
+				
+			<p>5. The extra dirt was a key buttress to the foundation. Buttress means…</p>
+				
+				<section id="indent">
+				
+					<input type = "radio" id = "q5" name = "q5" value = "Limiting factor."/>Limiting factor.<br>
+					<input type = "radio" id = "q5" name = "q5" value = "Support."/>Support.<br>
+					<input type = "radio" id = "q5" name = "q5" value = "Overwhelming condition."/>Overwhelming condition.<br>
+					<input type = "radio" id = "q5" name = "q5" value = "Obstacle."/>Obstacle.<br>
+				
+				</section>
+				<br>
+				
+			<p>6. The cathode of a battery was removed. Cathode means…</p>
+				
+				<section id="indent">
+					
+					<input type = "radio" id = "q6" name = "q6" value = "Positive pole."/>Positive pole.<br>
+					<input type = "radio" id = "q6" name = "q6" value = "Negative pole."/>Negative pole.<br>
+					<input type = "radio" id = "q6" name = "q6" value = "Neutral pole."/>Neutral pole.<br>
+					<input type = "radio" id = "q6" name = "q6" value = "Opposite pole."/>Opposite pole.<br>
+				
+				</section>
+				<br>
+				
+
+			<p>7. The doctor was known as a charlatan over the years of his practice. Charlatan means…</p>
+				
+				<section id="indent">
+					
+					<input type = "radio" id = "q7" name = "q7" value = "Quack."/>Quack.<br>
+					<input type = "radio" id = "q7" name = "q7" value = "Knowledgeable physician."/>Knowledgeable physician.<br>
+					<input type = "radio" id = "q7" name = "q7" value = "Procedural physician."/>Procedural physician.<br>
+					<input type = "radio" id = "q7" name = "q7" value = "Medical examiner."/>Medical examiner.<br>
+						
+				</section>
+				<br>
+				
+
+			<p>8. The wound exhibited signs of copious drainage requiring medical intervention. Copious means…</p>
+				
+				<section id="indent">
+				
+					<input type = "radio" id = "q8" name = "q8" value = "Minimal."/>Minimal.<br>
+					<input type = "radio" id = "q8" name = "q8" value = "Clear."/>Clear.<br>
+					<input type = "radio" id = "q8" name = "q8" value = "Maximal."/>Maximal.<br>
+					<input type = "radio" id = "q8" name = "q8" value = "Foul."/>Foul.<br>
+				
+				</section>
+				<br>
+				
+			<p>9. The attorney accused the witness of defaming the defendant. Defaming means…</p>
+				
+				<section id="indent">
+					
+					<input type = "radio" id = "q9" name = "q9" value = "Killing."/>Killing.<br>
+					<input type = "radio" id = "q9" name = "q9" value = "Badgering."/>Badgering.<br>
+					<input type = "radio" id = "q9" name = "q9" value = "Suffocating."/>Suffocating.<br>
+					<input type = "radio" id = "q9" name = "q9" value = "Slandering."/>Slandering.<br>
+				
+				</section>
+				<br>
+				
+			<p>10. The detective was able to derive the facts of the case. Derive means…</p>
+				
+				<section id="indent">
+					
+					<input type = "radio" id = "q10" name = "q10" value = "Desist."/>Desist.<br>
+					<input type = "radio" id = "q10" name = "q10" value = "Deter."/>Deter.<br>
+					<input type = "radio" id = "q10" name = "q10" value = "Devise."/>Devise.<br>
+					<input type = "radio" id = "q10" name = "q10" value = "Deduce."/>Deduce.<br>
+				
+				</section>
+				<br>
+				
+			<br>
+			
+			<button type = "submit" id = "submit" name = "submit" value = "submit">SUBMIT</button>
+			
+		</form>
+			
+	</div>
+	
+</body>
+
+</html>
